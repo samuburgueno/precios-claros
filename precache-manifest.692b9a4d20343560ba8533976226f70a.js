d31b3cf5f74ac10e99680ef0a483e7c565bb64cb
sburgue0 <sburgue0@rosario.gov.ar>
2019-02-22 09:29:21 -0300

    Updates

---
+++ precache-manifest.692b9a4d20343560ba8533976226f70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38da314c0f52f254b194bb0982bb063b",
+    "url": "/precios-claros/static/media/IMG_2934.38da314c.jpg"
+  },
+  {
+    "revision": "5506e155e34b66034e84",
+    "url": "/precios-claros/static/js/runtime~main.5506e155.js"
+  },
+  {
+    "revision": "92ed9e961c2d80e5c9e3",
+    "url": "/precios-claros/static/js/main.92ed9e96.chunk.js"
+  },
+  {
+    "revision": "b22d9f61114add85612b",
+    "url": "/precios-claros/static/js/2.b22d9f61.chunk.js"
+  },
+  {
+    "revision": "92ed9e961c2d80e5c9e3",
+    "url": "/precios-claros/static/css/main.e7fa261c.chunk.css"
+  },
+  {
+    "revision": "dbf9dbcacea6181d5fc560fb418870cf",
+    "url": "/precios-claros/index.html"
+  }
+];
