324c205d1834fe3e153fb72861d8e53fdee6b99c
sburgue0 <sburgue0@rosario.gov.ar>
2019-02-22 12:01:05 -0300

    Updates

---
+++ precache-manifest.5617b5ab11d33601802ad3a77db9e014.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5506e155e34b66034e84",
+    "url": "/precios-claros/static/js/runtime~main.5506e155.js"
+  },
+  {
+    "revision": "1d3224529bf74f4db102",
+    "url": "/precios-claros/static/js/main.1d322452.chunk.js"
+  },
+  {
+    "revision": "5313f2e4e708682d374c",
+    "url": "/precios-claros/static/js/2.5313f2e4.chunk.js"
+  },
+  {
+    "revision": "879f275f325107bff15b5b72a11b6f4d",
+    "url": "/precios-claros/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precios-claros/precache-manifest.67cf9f6d9624a92710835f07de3efc54.js"
+  "/precios-claros/precache-manifest.5617b5ab11d33601802ad3a77db9e014.js"
 );
 
 workbox.clientsClaim();
